7af823f1da2426b959ee151155af75cb0800439f
josehoyos25 <davidome2303@gmail.com>
2023-12-11 20:49:32 -0500

    porcentaje

--- js/amazon.js
+++ js/amazon.js
@@ -5,7 +5,7 @@ let calificacion4 = document.getElementById("calificacion4");
 let calificacion5 = document.getElementById("calificacion5");
 let btnGraficar = document.getElementById("btnGraficar");
 
-btnGraficar.addEventListener("click", function() {
+btnGraficar.addEventListener("click", function () {
     let sumaCalificacion = (
         Number(calificacion1.value) +
         Number(calificacion2.value) +
@@ -22,12 +22,12 @@ btnGraficar.addEventListener("click", function() {
         let promedio4Estrella = (Number(calificacion4.value) / sumaCalificacion) * 100;
         let promedio5Estrella = (Number(calificacion5.value) / sumaCalificacion) * 100;
 
-        // Mostrar resultados
-        console.log("Porcentaje 1 Estrella: " + promedio1Estrella.toFixed(2) + "%");
-        console.log("Porcentaje 2 Estrellas: " + promedio2Estrella.toFixed(2) + "%");
-        console.log("Porcentaje 3 Estrellas: " + promedio3Estrella.toFixed(2) + "%");
-        console.log("Porcentaje 4 Estrellas: " + promedio4Estrella.toFixed(2) + "%");
-        console.log("Porcentaje 5 Estrellas: " + promedio5Estrella.toFixed(2) + "%");
+        // Mostrar resultados en elementos HTML
+        document.getElementById("result1").textContent =  + promedio1Estrella.toFixed(2) + "%";
+        document.getElementById("result2").textContent =  + promedio2Estrella.toFixed(2) + "%";
+        document.getElementById("result3").textContent =  + promedio3Estrella.toFixed(2) + "%";
+        document.getElementById("result4").textContent =  + promedio4Estrella.toFixed(2) + "%";
+        document.getElementById("result5").textContent =  + promedio5Estrella.toFixed(2) + "%";
     } else {
         console.log("La suma de calificaciones es cero. No se puede calcular el porcentaje.");
     }
